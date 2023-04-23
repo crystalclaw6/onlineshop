9310dfc7ba021d73d9c54b8e7bb10512c1f89d46
sophie <bloxinas@list.ru>
2023-04-23 16:26:58 +0700

    добавлены изображения на главный экран сайта

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import MainPage from './mainpage';
 import NavJson from "./navigation.json";
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <nav className='navbar'> 
           <ul className='nav'>
+          <li className = "listItem">
+            <a className = "logo">Momo</a>
+            </li>
           {NavJson.map(chapter =>
             //<Link to= {chapter.name} className = "gamelinkStyle">
             <li className = "listItem">
-            <a className = "title"><MainPage title = {chapter.name} items = {chapter.parts}/></a>
+            <a className = "title"><MainPage title = {chapter.name} items = {chapter.parts} color = {chapter.color}/></a>
             </li>
            // </Link>
             )}
           </ul></nav>
       </header>
+      <body>
+      <div className='mosaic main'>
+            <div className='main box'>
+              <img className='photobox' src="./main1.jpg"></img>
+              <img className='photobox' src = "./main2.jpg"></img>
+              <button className='buttonNew'>НОВАЯ КОЛЛЕКЦИЯ</button>
+              </div>
+          </div>
+          
+      </body>
     </div>
   );
 }
--- src/mainpage.js
+++ src/mainpage.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import './mainpage.css';
 
 const MainPage= (props) => {
-    const [open, setIsOpen] = useState(false)
+    const [ close, setIsClose] = useState(false)
     const rows = [];
     
 for (let i = 0; i < props.items.length; i++) {
     rows.push(<li key = {i}>{props.items[i]}</li>);
 }
-console.log(rows.length)
+useEffect(() => {
+  console.log(rows.length);
+  if (rows.length === 0) {setIsClose(true);}
+}, []);
+console.log(close);
 return(
-    <div className="navbar">
-    <span onMouseEnter={()=>setIsOpen(true)} onMouseLeave={()=>setIsOpen(false)}>{props.title}</span>
-  <div className={`menu ${open ? 'open' : ''}`}>
+    <div className="bar" >
+    <span className = {props.color}>{props.title}</span>
+  <div id="i" className={`menu ${close ? 'close' : ''}`}>
     <ul>
       {rows}
     </ul>
+    <div className="itemphotos"><img className="photo" src="./hello.jpg"></img></div>
   </div>
   </div>
 )
